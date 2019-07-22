024d7f7c08797fae4be0a3e36063bb7760400f8b
vision-dev <obenchgra@vision-business-consulting.com>
2019-07-22 17:33:49 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { IUser } from "./Models/user";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+
+    const user: IUser = {
+        lastname: "ben",
+        name: "ouael",
+
+    };
+    res.setHeader("Content-Type", "application/json");
+    res.send(user);
+});
+
+app.listen(port, () => {
+    console.log(`Server listen on ${port}`);
+});
